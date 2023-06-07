9ca004efdc88ba106dac6c76e1f33dea9140cdef
Holme.A <homer.asava@gmail.com>
2023-06-07 21:51:26 +0700

    revise permission

--- config/permissions.py
+++ config/permissions.py
@@ -1,29 +1,15 @@
 from rest_framework.permissions import BasePermission
 
-__all__ = ["IsAuthenticated", "IsMaid", "IsSupervisor", "IsSuperuser"]
+__all__ = ["IsAuthenticated", "AllowAny"]
+
+
+class AllowAny(BasePermission):
+    def has_permission(self, request, view):
+        return True
 
 
 class IsAuthenticated(BasePermission):
     def has_permission(self, request, view):
         return bool(
             request.user and request.user.is_active and request.user.is_authenticated
-        )
-
-
-class IsSuperuser(BasePermission):
-    def has_permission(self, request, view):
-        return bool(
-            request.user and request.user.is_active and request.user.is_superuser
-        )
-
-
-class IsMaid(BasePermission):
-    def has_permission(self, request, view):
-        return bool(request.user and request.user.is_active and request.user.is_maid)
-
-
-class IsSupervisor(BasePermission):
-    def has_permission(self, request, view):
-        return bool(
-            request.user and request.user.is_active and request.user.is_supervisor
         )
