22046f5ec6cf1b31f5aca6ddf50b62da0e46c220
Holme.A <homer.asava@gmail.com>
2023-06-10 22:31:16 +0700

    remove reorder logic

--- memo_manager/models.py
+++ memo_manager/models.py
@@ -5,6 +5,7 @@ from django.apps import apps
 from django.db import models
 from django.db.models import Max
 from user_manager.models import User
+from django.db.models import Q
 
 
 class Memo(models.Model):
@@ -24,21 +25,6 @@ class MemoItem(models.Model):
     order = models.SmallIntegerField(default=-1)
 
     def save(self, **kwargs):
-        if (
-            MemoItem.objects.filter(parent_memo=self.parent_memo, order=self.order)
-            .exclude(id=self.id)
-            .exists()
-        ):
-            reorder_item = (
-                MemoItem.objects.filter(
-                    parent_memo=self.parent_memo, order__gte=self.order
-                )
-                .exclude(id=self.id)
-                .order_by("-order")
-            )
-            for instance in reorder_item:
-                instance.order += 1
-                instance.save()
         if self.order == -1:
             max_value = (
                 MemoItem.objects.filter(parent_memo=self.parent_memo)
