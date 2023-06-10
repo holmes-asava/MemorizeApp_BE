4adb4a44cca0aa694fe6c222f5bedcc5b9b4ccdc
Holme.A <homer.asava@gmail.com>
2023-06-10 23:17:16 +0700

    update reorder logic in serializer

--- memo_manager/serializers.py
+++ memo_manager/serializers.py
@@ -7,6 +7,7 @@ class MemoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = MemoItem
         fields = ["id", "is_completed", "description", "order"]
+        read_only = ["id"]
         ordering = [
             "order",
         ]
@@ -14,6 +15,46 @@ class MemoItemSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data["parent_memo"] = self.context["parent_memo"]
         return super().create(validated_data)
+
+    def reorder_other_item(self, target_order, current_order, parent_memo):
+        if current_order < target_order:
+            decrease_reorder_item = MemoItem.objects.filter(
+                parent_memo=parent_memo,
+                order__gt=current_order,
+                order__lte=target_order,
+            ).order_by("-order")
+            for instance in decrease_reorder_item:
+                instance.order -= 1
+                instance.save()
+        else:
+            if MemoItem.objects.filter(
+                parent_memo=parent_memo, order=target_order
+            ).exists():
+                increase_reorder_item = MemoItem.objects.filter(
+                    parent_memo=parent_memo, order__gte=target_order
+                ).order_by("-order")
+                for instance in increase_reorder_item:
+                    instance.order += 1
+                    instance.save()
+
+    def update(self, instance, validated_data):
+        self.reorder_other_item(
+            target_order=validated_data["order"],
+            current_order=instance.order,
+            parent_memo=instance.parent_memo,
+        )
+
+        return super().update(instance, validated_data)
+
+
+class ReOrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MemoItem
+        fields = ["id", "is_completed", "description", "order"]
+        read_only = ["id", "is_completed", "description"]
+        ordering = [
+            "order",
+        ]
 
 
 class MemoSerializer(serializers.ModelSerializer):
